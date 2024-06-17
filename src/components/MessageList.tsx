fbcb36f4f22ad0d62bb9f60696e839e38a8831b2
mxrsel <marsel14082006@gmail.com>
2024-06-17 21:24:11 +0600

    finish component for message list

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import {Message} from '../App.tsx';
 
-const MessageList = () => {
+interface Props{
+  messages: Message[];
+}
+
+const MessageList: React.FC<Props> = ({messages}) => {
   return (
-    <div>
-      
+    <div className='messageList'>
+      {messages.map((message) => (
+        <div key={message.id} className='message'>
+          <strong>{message.author}:</strong> {message.message}
+          <br/>
+          <small>{new Date(message.datetime).toLocaleString()}</small>
+        </div>
+      ))}
     </div>
   );
 };
